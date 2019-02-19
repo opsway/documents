99b0b3b6256469be9ffefb42315c18ff04e455ad
Ilyar <ilyar.software@gmail.com>
2019-02-19 11:21:53 +0200

    test: add cover for util.Getenv

--- util/os_test.go
+++ util/os_test.go
@@ -2,10 +2,17 @@ package util
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"os"
 	"testing"
 )
 
 func TestOs(t *testing.T) {
+	Convey("Getenv", t, func() {
+		So(Getenv("NO_SET_VAR", "foo"), ShouldEqual, "foo")
+		os.Setenv("SET_VAR", "bar")
+		So(Getenv("SET_VAR", "foo"), ShouldEqual, "bar")
+	})
+
 	Convey("IsValidDir", t, func() {
 		So(IsValidDir("../tests"), ShouldBeTrue)
 		So(IsValidDir("tests"), ShouldBeFalse)
